b14f6e582833dcbb2c8d940b418baba1ae2b7146
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2022-07-01 14:54:25 +0900

    init: first commit

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod hello;
+use hello::hello;
+fn main() {
+    hello();
+    println!("Hello, world!");
+}
---
+++ src/server.rs
@@ -0,0 +1,5 @@
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
